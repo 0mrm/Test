34992f2a43531dc1704d25bc5cfc9e7ea8176007
0mrm <13brm00@gmail.com>
2023-03-13 16:44:44 +0100

    sumar

--- Clase.java
+++ Clase.java
@@ -1,5 +1,10 @@
 public class Clase {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println(sumar(10,5));
+    }
+
+    public static int sumar(int x, int y) {
+        return x + y;
     }
 }
